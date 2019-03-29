b2c49dbc0fe2cd6cc5bb9c1ca4b11d355871de5e
Dean Cazes <dincaz2@gmail.com>
2019-03-29 15:01:59 +0300

    Changed time recorded to datetime

--- speed_calculator.py
+++ speed_calculator.py
@@ -1,3 +1,7 @@
 import pandas as pd
+from datetime import datetime
 
-pd.read_csv()
+df = pd.read_csv('datasets/shaul_hamelech_routes_2019-03-17_2019-03-23.csv')
+df['time_recorded_datetime'] = df.apply(lambda row: row['timestamp'].split('T')[0] + " "+ row["time_recorded"],axis=1)
+df['time_recorded_datetime'] = pd.to_datetime(df['time_recorded_datetime'])
+df.to_csv('datasets/shaul_hamelech_routes_2019-03-17_2019-03-23_update.csv')
