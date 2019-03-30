cc37a2c6cd2cd34fe642c5fca0548dfebd91584a
Dean Cazes <dincaz2@gmail.com>
2019-03-30 10:45:31 +0300

    speed

--- speed_calculator.py
+++ speed_calculator.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import geopy.distance
 import numpy as np
+from datetime import datetime, timedelta
 
 # df = pd.read_csv('datasets/shaul_hamelech_routes_2019-03-17_2019-03-23.csv')
 # df['time_recorded_datetime'] = pd.to_datetime(df['time_recorded_datetime'])
@@ -23,22 +24,46 @@ def route_distance(route):
         route_length += abs(geopy.distance.geodesic(route[i], route[i+1]).m)
     return route_length
 
-def found_nearest_dot(route_dots_list, dot):
-    index = np.argmin([abs(geopy.distance.geodesic(dot, dot2).m) for dot2 in route_dots_list])
-    return route_dots_list[index]
+def find_nearest_dot(route_dots_list, dot, epsilon=30):
+    diffs = [abs(geopy.distance.geodesic(dot, dot2).m) for dot2 in route_dots_list]
+    index = np.argmin(diffs)
+    if diffs[index] < epsilon:
+        return route_dots_list[index]
 
-def calc_speed(df, segment, start_time, end_time):
-    start_point, end_point = segment[0], segment[-1]
-    dist = route_distance(segment)
-    # df = 1 # time interval
-    trips = df.groupby('trip_id_to_date')
+def find_nearest_dot(route_dots_list, lat, lon, epsilon=30):
+    diffs = [abs(geopy.distance.geodesic((lat,lon), dot2).m) for dot2 in route_dots_list]
+    index = np.argmin(diffs)
+    if diffs[index] < epsilon:
+        return route_dots_list[index]
+
+def calc_speed_in_interval(df, segments_df):
+    trips = df.groupby('trip_id_to_date', 'segment')
     for _, g in trips:
-        df_group = df[df['time_recorded_datetime'].between(start_time, end_time)]
+        g = g['time_recorded_datetime'].sort_values()
+        time = g[-1] - g[0]
+        segment = segments_df[segments_df.name == ''] #TODO
+        dist = route_distance(segment)
+        return dist / time
 
+
+
+def find_data_time(minutes, date, df, segments):
+    df = df[pd.to_datetime(df['time_recorded_dataTime']).dt.date == date]
+    curDate = df["time_recorded_datetime"].min()
+    nextDate = curDate + timedelta(minutes = minutes)
+    end_date =  df["time_recorded_datetime"].max()
+    dic_times  = pd.DataFrame(columns=('start_interval', 'end_interval', 'uniqueBus'))
+    while nextDate < end_date:
+        current_data = df[df['time_recorded_datetime'].between(curDate,nextDate)]
+        calc_speed_in_interval(current_data, segments)
+        curDate = nextDate
+        nextDate = curDate + timedelta(minutes = minutes)
+    current_data = df[df['time_recorded_datetime'].between(curDate, end_date)]
+    calc_speed_in_interval(current_data, segments)
 
 origin = route_dots_list[0]
 dest = route_dots_list[-1]
 print(route_distance(route_dots_list))
 
 dot = (32.098752, 34.7794)
-print(found_nearest_dot(route_dots_list, dot))
+print(find_nearest_dot(route_dots_list, dot))
